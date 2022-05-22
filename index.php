04fc99cfba9e1c583a52ffc312c0a056de1966cd
ntwarib <bertinntwari5@gmail.com>
2022-05-22 18:37:48 +0200

    first commit

--- index.php
+++ index.php
@@ -30,7 +30,7 @@
                 <div class="col-md-12">
                     <div class="mt-5 mb-3 clearfix">
                         <h2 class="pull-left">Bertin Details</h2>
-                        <a href="create.php" class="btn btn-success pull-right"><i class="fa fa-plus"></i> Add New Employee</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a href="create.php" class="btn btn-success pull-right"><i class="fa fa-plus"></i> Upload csv file</a>
+                        <a href="create.php" class="btn btn-success pull-right"><i class="fa fa-plus"></i> Add New Employee</a>
                     </div>
                     <?php
                     // Include config file
